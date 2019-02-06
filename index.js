de0cf9c60ac4a39db4206925b56d69adfab015b2
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-02-06 22:57:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
 const driver = {}
 function updateDriverWithKeyAndValue(obj, key, value) {
   const newDriver = {...obj };
-
   newDriver[key] = value;
   return newDriver;
+}
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver[key] = value;
+  return driver;
 }
